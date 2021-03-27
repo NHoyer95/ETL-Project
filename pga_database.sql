3f4c920c7edfb9cb89ce911f0b69d928b197e925
Eric Shaffer <EricShaffer@Chriss-MacBook-Pro-3.local>
2021-03-27 10:44:55 -0500

    updated sql database

--- pga_database.sql
+++ pga_database.sql
@@ -1,21 +1,24 @@
+drop table pga_tour_stats_2017, pga_tour_stats_2018
+
+
 create table pga_tour_stats_2017(
 	player varchar primary key,
-	rounds_played int,
-	points int,
-	avg_score decimal(10,2),
-	wins int,
-	top_tens int,
-	avg_carry_distance decimal(6,2),
-	sg_putting_per_round decimal(6,6)
+	rounds_played_17 float,
+	points_17 float,
+	avg_score_17 float,
+	wins_17 float,
+	top_tens_17 float,
+	avg_carry_distance_17 float,
+	sg_putting_per_round_17 float
 );
 
 create table pga_tour_stats_2018(
 	player varchar primary key,
-	rounds_played int,
-	points int,
-	avg_score decimal(10,2),
-	wins int,
-	top_tens int,
-	avg_carry_distance decimal(6,2),
-	sg_putting_per_round decimal(6,6)
+	rounds_played_18 float,
+	points_18 float,
+	avg_score_18 float,
+	wins_18 float,
+	top_tens_18 float,
+	avg_carry_distance_18 float,
+	sg_putting_per_round_18 float
 );
